e3b721d8eb1d3c0e37c9226697e59df0f3f43aff
tsl <taosiliang1127tsl@163.com>
2018-02-01 22:48:45 +0800

    优化成有结构的参数

--- main.js
+++ main.js
@@ -5,12 +5,25 @@ window.jQuery = function(nodeOrSelector){
     return nodes
 }
 
-window.jQuery.ajax=function(url,method,body,successFn,failFn){    //要加路径、方法、第四部分内容
+window.jQuery.ajax=function(options){ 
+    let url = options.url   
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
+
+
     let request = new XMLHttpRequest()    
     request.open(
         method,   //也可以改成POST/PUT/DELETE等
         url
 ) //配置request
+
+    for (let key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key,value)
+    }
 
 request.onreadystatechange = ()=>{
     if(request.readyState===4){
@@ -27,14 +40,35 @@ request.onreadystatechange = ()=>{
 
 window.$=window.jQuery
 
-
+function f1(responseText){}
+function f2(responseText){}
 
 
 myButton.addEventListener('click',(e)=>{
-    window.jQuery.ajax(
-        '/xxx',
-        'POST',
-        'a=1&b=2',
-        (responseText)=>{console.log(1)},
-        (request)=>{console.log(2)})
+    // let obj ={
+    //     url:'/xxx',
+    //     method:'GET',
+    //     successFn:()=>{},
+    //     failFn:()=>{}
+    // }
+    // window.jQuery.ajax(obj)
+
+    window.jQuery.ajax({
+        url:'/ff',
+        method:'GET',
+        headers: {
+            'content-type':'application/x-www-form-urlencoded',
+            'ff':'18'
+        },
+        successFn:(x)=>{
+            console.log(x)    //x其实是callback回来的request.responseText
+            f1.call(undefined,x)   //x其实是callback回来的responseText（上面的f1里面的）
+            f2.call(undefined,x)   //x其实是callback回来的responseText（上面的f2里面的）
+        },
+        failFn:(x)=>{
+            console.log(x)  //x其实是callback回来的request
+            console.log(x.status)
+            console.log(x.responseText)
+        }
+    })
 })
