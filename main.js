af1f4989eabbae9eb759d9adea423ed84a08ee63
tsl <taosiliang1127tsl@163.com>
2018-02-01 23:32:34 +0800

    用jQuery库来写

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
-window.jQuery = function(nodeOrSelector){
-    let nodes={}
-    nodes.addClass = function(){}
-    nodes.html = function(){}
-    return nodes
-}
+// window.jQuery = function(nodeOrSelector){
+//     let nodes={}
+//     nodes.addClass = function(){}
+//     nodes.html = function(){}
+//     return nodes
+// }
 
-window.jQuery.ajax=function({url,method,body,successFn,failFn,headers}){   //本来是options
-    // let url
-    // if(arguments.length===1){
-    //     url = options.url   
-    // }else if(arguments.length===2){
-    //     url = arguments[0]
-    //     options = arguments[1]
-    // }
+// window.jQuery.ajax=function({url,method,body,successFn,failFn,headers}){   //本来是options
+//     // let url
+//     // if(arguments.length===1){
+//     //     url = options.url   
+//     // }else if(arguments.length===2){
+//     //     url = arguments[0]
+//     //     options = arguments[1]
+//     // }
 
-    // let method = options.method
-    // let body = options.body
-    // let successFn = options.successFn
-    // let failFn = options.failFn
-    // let headers = options.headers
- //ES6来写   解构赋值
-//  let {url,method,body,successFn,failFn,headers} =options   //与上面的参数等价替换
+//     // let method = options.method
+//     // let body = options.body
+//     // let successFn = options.successFn
+//     // let failFn = options.failFn
+//     // let headers = options.headers
+//  //ES6来写   解构赋值
+// //  let {url,method,body,successFn,failFn,headers} =options   //与上面的参数等价替换
 
 
 
-    let request = new XMLHttpRequest()    
-    request.open(
-        method,   //也可以改成POST/PUT/DELETE等
-        url
-) //配置request
+//     let request = new XMLHttpRequest()    
+//     request.open(
+//         method,   //也可以改成POST/PUT/DELETE等
+//         url
+// ) //配置request
 
-    for (let key in headers){
-        let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
+//     for (let key in headers){
+//         let value = headers[key]
+//         request.setRequestHeader(key,value)
+//     }
 
-request.onreadystatechange = ()=>{
-    if(request.readyState===4){
-        if(request.status>=200&&request.status<300){
-            successFn.call(undefined,request.responseText) 
-        }else if(request.status>=400){
-            failFn.call(undefined,request)
-        }
-    }
-}
+// request.onreadystatechange = ()=>{
+//     if(request.readyState===4){
+//         if(request.status>=200&&request.status<300){
+//             successFn.call(undefined,request.responseText) 
+//         }else if(request.status>=400){
+//             failFn.call(undefined,request)
+//         }
+//     }
+// }
    
-    request.send(body)
-}
+//     request.send(body)
+// }
 
-window.$=window.jQuery
+// window.$=window.jQuery
 
 function f1(responseText){}
 function f2(responseText){}
@@ -66,16 +66,12 @@ myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
         url:'/ff',
         method:'GET',
-        headers: {
-            'content-type':'application/x-www-form-urlencoded',
-            'ff':'18'
-        },
-        successFn:(x)=>{
+        success:(x)=>{
             console.log(x)    //x其实是callback回来的request.responseText
             f1.call(undefined,x)   //x其实是callback回来的responseText（上面的f1里面的）
             f2.call(undefined,x)   //x其实是callback回来的responseText（上面的f2里面的）
         },
-        failFn:(x)=>{
+        error:(x)=>{
             console.log(x)  //x其实是callback回来的request
             console.log(x.status)
             console.log(x.responseText)
